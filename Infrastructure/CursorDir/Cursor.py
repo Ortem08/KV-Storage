2eef0d7cc188e9a01a4c75521084bdbe1abcd547
Ortem08 <72299038+Ortem08@users.noreply.github.com>
2023-10-08 14:29:30 +0500

    FIX PR4-3

--- Infrastructure/CursorDir/Cursor.py
+++ Infrastructure/CursorDir/Cursor.py
@@ -7,8 +7,8 @@ class Cursor(ICursor):
     len_size = 16
 
     def __init__(self, index=0, len=0):
-        if (len < 0 or index < 0 or index > 2**Cursor.index_size
-                or len > 2**Cursor.len_size):
+        if (len < 0 or index < 0 or index > 10*Cursor.index_size
+                or len > 10**Cursor.len_size):
             raise ValueError
 
         self._type = 'single'
