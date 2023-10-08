94c33dd031cc52949b2b469b030a663e8ba7b639
Dmitriy Smirnov <d.smirnov@skbkontur.ru>
2023-10-08 19:39:49 +0500

    Add response

---
+++ KVStorageResponse.py
@@ -0,0 +1,16 @@
+import json
+
+class KVStorageResponse:
+    def __init__(self, key: str, value: str, error: str):
+        self._key = key
+        self._value = value
+        self._error = error
+
+    def to_json(self):
+        return json.dumps({"key": self._key, "value": self._value, "error": self._error})
+
+    @staticmethod
+    def from_json(json_str: str):
+        values = json.loads(json_str)
+
+        return KVStorageResponse(values["key"], values["value"], values["error"])
